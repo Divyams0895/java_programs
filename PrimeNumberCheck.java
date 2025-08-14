1ae2dcd20912c5f9629d00250c96dea2fa4ff41a
Divya M S <48670337+Divyams0895@users.noreply.github.com>
2025-08-14 11:47:14 +0530

    Update PrimeNumberCheck.java

--- PrimeNumberCheck.java
+++ PrimeNumberCheck.java
@@ -1,12 +1,26 @@
 import java.util.Scanner;
 
 public class PrimeNumberCheck {
-    public static void main(String[] args) {
-        int number;
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter a number");
 
-        number = sc.nextInt();
+    public void checkPrimeMethod1(int number){
+        int temp;
+        boolean isPrime = true;
+      
+        for (int i = 2; i<= number/2; i++) {
+            temp = number%i;
+            if (temp == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) 
+            System.out.println(number + " is prime");
+            else
+                System.out.println(number + " is not a prime");
+    }
+
+    public void checkPrimeMethod2(int number){
+       
         boolean isPrime=true;
 
         if(number<=1){
@@ -26,5 +40,17 @@ public class PrimeNumberCheck {
         }else{
              System.out.println("The given number "+number+" is not prime");
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int number = sc.nextInt();
+        PrimeNumberCheck pcheck = new PrimeNumberCheck();
+        pcheck.checkPrimeMethod1(number);
+
+        pcheck.checkPrimeMethod2(number);
+        sc.close();
+        
     }
 }
